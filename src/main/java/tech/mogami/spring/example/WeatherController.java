c2a07a48ae793bd4f75983b393130d25c65e0ce8
straumat <github.backlog426@passmail.net>
2025-05-27 16:15:12 +0200

    Moving to 0.1.2

--- src/main/java/tech/mogami/spring/example/WeatherController.java
+++ src/main/java/tech/mogami/spring/example/WeatherController.java
@@ -2,20 +2,20 @@ package tech.mogami.spring.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tech.mogami.spring.autoconfigure.annotation.X402PaymentRequirement;
+import tech.mogami.spring.autoconfigure.annotation.X402PaymentRequirements;
 
 @RestController
 public class WeatherController {
 
-    @X402PaymentRequirement(
+    @X402PaymentRequirements(
             scheme = "exact",
             network = "base-sepolia",
             maximumAmountRequired = "1000",
             payTo = "0x7553F6FA4Fb62986b64f79aEFa1fB93ea64A22b1",
             asset = "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
             extra = {
-                    @X402PaymentRequirement.ExtraEntry(key = "name", value = "USDC"),
-                    @X402PaymentRequirement.ExtraEntry(key = "version", value = "2")
+                    @X402PaymentRequirements.ExtraEntry(key = "name", value = "USDC"),
+                    @X402PaymentRequirements.ExtraEntry(key = "version", value = "2")
             }
     )
     @GetMapping("/weather")
