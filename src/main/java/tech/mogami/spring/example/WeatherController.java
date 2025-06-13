e44c4a5a5ff6cab28b55705e5b2c8bb98ce21546
straumat <github.backlog426@passmail.net>
2025-06-13 11:11:37 +0200

    Moving to latest releases

--- src/main/java/tech/mogami/spring/example/WeatherController.java
+++ src/main/java/tech/mogami/spring/example/WeatherController.java
@@ -10,7 +10,7 @@ public class WeatherController {
     @X402PaymentRequirements(
             scheme = "exact",
             network = "base-sepolia",
-            maximumAmountRequired = "1000",
+            maximumAmountRequired = "3000",
             payTo = "0x7553F6FA4Fb62986b64f79aEFa1fB93ea64A22b1",
             asset = "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
             extra = {
